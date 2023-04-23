9afaa8ce43339b1411443c798c8a2804b0c2ed49
pl712 <liuzhengyao2011@gmail.com>
2023-04-22 21:50:04 -0500

    update

--- openai/categorize.py
+++ openai/categorize.py
@@ -7,9 +7,10 @@ api_key = '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'
 '''
 from categorize import getSinglePrediction
 getSinglePrediction(api_key, 'This is a test tweet') -> string of category
+getListPrediction(api_key, ['This is a test tweet', 'This is another test tweet']) -> list of categories
 '''
 
-def getSinglePrediction(apiKey, content):
+def getSinglePrediction(apiKey, content:str) -> str:
   openai.api_key = apiKey
 
   lstOfCategories = ['web3 activities and events', 'web3 announcements', 'web3 research output', 'web3 meme', 'crypto and markets', 'web3 phishing or irrelevant', 'unknown']
@@ -27,6 +28,29 @@ def getSinglePrediction(apiKey, content):
   result = response['choices'][0]['text']
 
   return result[-len(result)+1:]
+
+def getListPrediction(apiKey, contentList:list) -> list:
+    openai.api_key = apiKey
+    returnLst = []
+
+    lstOfCategories = ['web3 activities and events', 'web3 announcements', 'web3 research output', 'web3 meme', 'crypto and markets', 'web3 phishing or irrelevant', 'unknown']
+
+    for i in contentList:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"Decide which category the Tweet best classify as {lstOfCategories}.\n\nTweet: \"{i}\"\nCategory: ",
+            temperature=0,
+            max_tokens=60,
+            top_p=1,
+            frequency_penalty=0.5,
+            presence_penalty=0
+        )
+
+        result = response['choices'][0]['text']
+
+        returnLst.append(result[-len(result)+1:])
+
+    return returnLst
 
 def getPredictionDF(apiKey, csv_path, return_path = 'categorized.csv'):
   openai.api_key = apiKey
